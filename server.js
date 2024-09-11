64e2c9d109199d4738134d4050b32e4e300b06eb
Afterlife24 <afterlife668@gmail.com>
2024-09-12 02:22:02 +0530

    serverless

--- server.js
+++ server.js
@@ -36,10 +36,12 @@ app.post("/markAsDelivered", async (req, res) => {
     }
 });
 
-const PORT = 5000;
-app.listen(PORT, function() { 
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = 5000;
+// app.listen(PORT, function() { 
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+module.exports = app;
 
 
 
